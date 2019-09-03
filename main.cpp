68db258781116a21438a00d0a0ce04e204555c88
feldmatias <feldmatias@gmail.com>
2019-09-03 17:46:44 -0300

    removed unused imports

--- main.cpp
+++ main.cpp
@@ -3,8 +3,6 @@
 #include <iostream>
 #include "src/config/ActorsCreator.h"
 #include "src/comunication/ExitComunicator.h"
-#include "src/concurrency/Fifo.h"
-#include "src/utils/file/WriteOnlyFile.h"
 
 #define EXIT_OK 0
 
