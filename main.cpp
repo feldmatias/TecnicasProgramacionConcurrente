988e58e86f3d0ea7bc0723fb20e9edf0871366bc
feldmatias <feldmatias@gmail.com>
2019-09-04 18:25:43 -0300

    made flowertype hashable

--- main.cpp
+++ main.cpp
@@ -1,6 +1,5 @@
 #include <sys/wait.h>
 #include <unistd.h>
-#include <iostream>
 #include "src/config/ActorsCreator.h"
 #include "src/comunication/ExitComunicator.h"
 
@@ -16,9 +15,8 @@ int main() {
     exitComunicator.start();
 
     while (pid_t pid = wait(NULL) > 0) {
-        std::cout << "Process finished: " << pid << std::endl;
+        // Child process finished
     }
 
-    std::cout << "Parent finished: " << getpid() << std::endl;
     return EXIT_OK;
 }
--- src/flower/FlowerType.h
+++ src/flower/FlowerType.h
@@ -1,16 +1,17 @@
-
-
 #ifndef PRIMAVERA_CONCURRENTE_FLOWERTYPE_H
 #define PRIMAVERA_CONCURRENTE_FLOWERTYPE_H
 
 
 #include <string>
+#include <vector>
 
 class FlowerType {
 private:
     std::string type;
 
 public:
+    static std::vector<FlowerType> all();
+
     static FlowerType Tulip();
 
     static FlowerType Rose();
@@ -20,6 +21,19 @@ public:
     ~FlowerType();
 
     friend std::ostream& operator<<(std::ostream& os, const FlowerType& flowerType);
+
+    bool operator==(const FlowerType &other) const;
+
+    std::string getType() const;
 };
+
+namespace std {
+    template <>
+    struct hash<FlowerType> {
+        std::size_t operator()(const FlowerType& key) const {
+            return hash<string>()(key.getType());
+        }
+    };
+}
 
 #endif //PRIMAVERA_CONCURRENTE_FLOWERTYPE_H
