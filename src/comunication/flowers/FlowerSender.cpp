df77db9f636660892b97476fb275e6883924c37b
feldmatias <feldmatias@gmail.com>
2019-09-06 19:21:20 -0300

    added transaction sender

--- src/comunication/flowers/FlowerSender.cpp
+++ src/comunication/flowers/FlowerSender.cpp
@@ -4,12 +4,21 @@
 #include "../../utils/file/WriteOnlyFile.h"
 #include "../../concurrency/Fifo.h"
 
+void send(const std::string &receiver, const std::string &info) {
+    WriteOnlyFile file(Fifo::filename(receiver));
+    file.writeLine(info);
+}
+
 FlowerSender::FlowerSender() = default;
 
 FlowerSender::~FlowerSender() = default;
 
 void FlowerSender::sendFlowers(const std::string &receiver, const FlowerList& flowers) const {
     std::string info = protocol.sendFlowers(flowers);
-    WriteOnlyFile file(Fifo::filename(receiver));
-    file.writeLine(info);
+    send(receiver, info);
+}
+
+void FlowerSender::sendFlowerTransaction(const std::string& receiver, const FlowerTransaction& transaction) const {
+    std::string info = protocol.sendFlowerTransaction(transaction);
+    send(receiver, info);
 }
