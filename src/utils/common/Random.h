a1eb39b8ab4a7a88570ba0e18637f1427b0ad3cf
feldmatias <feldmatias@gmail.com>
2019-09-07 17:28:48 -0300

    created client generator

--- src/utils/common/Random.h
+++ src/utils/common/Random.h
@@ -11,6 +11,16 @@ public:
      * Generate a random number between 0 and max - 1.
      */
     static int generate(int max);
+
+    /**
+     * Generate a random number between min and max.
+     */
+    static int generate(int min, int max);
+
+    /**
+     * Calculate a random and return if is below probability.
+     */
+    static bool probability(int probability);
 };
 
 
