118b7727c31ce3382d0a83f060d8f505f8691aec
feldmatias <feldmatias@gmail.com>
2019-09-07 20:33:31 -0300

    added doc to config

--- src/config/ActorsConfig.h
+++ src/config/ActorsConfig.h
@@ -7,18 +7,39 @@
 
 class ActorsConfig : public Config {
 public:
+    /**
+     * Constructor.
+     */
     ActorsConfig();
 
+    /**
+     * Destructor.
+     */
     ~ActorsConfig() override;
 
+    /**
+     * Get number of producers.
+     */
     int numberOfProducers() const;
 
+    /**
+     * Get producer's box size.
+     */
     int producersBoxSize() const;
 
+    /**
+     * Get number of distribution centers.
+     */
     int numberOfDistributionCenters() const;
 
+    /**
+     * Get distribution center's box size.
+     */
     int centersBoxSize() const;
 
+    /**
+     * Get number of points of sale.
+     */
     int numberOfPointsOfSale() const;
 };
 
