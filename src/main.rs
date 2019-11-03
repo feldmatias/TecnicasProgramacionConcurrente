638808e5105c57c90d53e235bc86205662786e57
feldmatias <feldmatias@gmail.com>
2019-11-03 17:37:40 -0300

    simple example with objects

--- src/main.rs
+++ src/main.rs
@@ -11,28 +11,101 @@ fn main() {
 
     for i in 0..MINERS {
 
-        let clone = condvar.clone();
+        let signal = condvar.clone();
 
         let t = thread::spawn(move|| {
-            let (mutex, condv) = &*clone;
-
-            let started = mutex.lock().unwrap();
-            condv.wait(started).unwrap();
-
-            println!("Thread {} started", i);
+            let miner = Miner::create(&signal, i);
+            miner.start();
         });
 
         threads.push(t);
     }
 
-    thread::sleep(Duration::from_secs(1));
-    println!("Main notify threads");
+    let leader = Leader::create(&condvar.clone());
+    leader.start();
 
-    let (_lock, cvar) = &*condvar;
-    cvar.notify_all();
-
+    println!("Main: waiting to join");
     for thread in threads {
         thread.join().unwrap();
     }
 
+}
+
+// of course this goes to another file
+struct Miner {
+    start_signal: LeaderSignal,
+    number: i32
+}
+
+impl Miner {
+    fn create(signal: &Arc<(Mutex<bool>, Condvar)>, i : i32) -> Miner {
+        let signal = LeaderSignal {
+            signal: signal.clone()
+        };
+
+        let miner = Miner {
+            start_signal: signal,
+            number: i
+        };
+
+        return miner;
+    }
+
+    fn start(&self) {
+        self.start_signal.wait();
+
+        thread::sleep(Duration::from_millis(100));
+        println!("Miner {} started and did work", self.number);
+    }
+}
+
+// of course this goes to another file
+struct Leader {
+    start_signal: LeaderSignal
+}
+
+impl Leader {
+
+    fn create(signal: &Arc<(Mutex<bool>, Condvar)>) -> Leader {
+        let signal = LeaderSignal {
+            signal: signal.clone()
+        };
+
+        let leader = Leader {
+            start_signal: signal
+        };
+
+        return leader;
+    }
+
+    fn start(&self) {
+        thread::sleep(Duration::from_secs(1));
+        println!("Leader notify miners");
+        self.start_signal.signal();
+    }
+}
+
+// of course this goes to another file
+struct LeaderSignal {
+    signal: Arc<(Mutex<bool>, Condvar)>,
+}
+
+impl LeaderSignal {
+    fn wait(&self) {
+        let (ref mutex, ref condv) = *self.signal;
+
+        let mut started = mutex.lock().unwrap();
+
+        while !*started {
+            started = condv.wait(started).unwrap();
+        }
+    }
+
+    fn signal(&self) {
+        let (ref mutex, ref condv) = *self.signal;
+
+        let mut started = mutex.lock().unwrap();
+        *started = true;
+        condv.notify_all();
+    }
 }
