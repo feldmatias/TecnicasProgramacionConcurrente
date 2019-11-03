7430e7e265995c872918c3a779626021b69fe23d
feldmatias <feldmatias@gmail.com>
2019-11-03 16:49:42 -0300

    simple example with condvar and threads

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,38 @@
+use std::sync::{Arc, Condvar, Mutex};
+use std::thread;
+use std::time::Duration;
+
 fn main() {
-    println!("Hello, world!");
+    let condvar = Arc::new((Mutex::new(false), Condvar::new()));
+
+    const MINERS : i32 = 5;
+
+    let mut threads = vec![];
+
+    for i in 0..MINERS {
+
+        let clone = condvar.clone();
+
+        let t = thread::spawn(move|| {
+            let (mutex, condv) = &*clone;
+
+            let started = mutex.lock().unwrap();
+            condv.wait(started).unwrap();
+
+            println!("Thread {} started", i);
+        });
+
+        threads.push(t);
+    }
+
+    thread::sleep(Duration::from_secs(1));
+    println!("Main notify threads");
+
+    let (_lock, cvar) = &*condvar;
+    cvar.notify_all();
+
+    for thread in threads {
+        thread.join().unwrap();
+    }
+
 }
